0497e4a8758b0837fec79c8b368a3be2f8acdb90
CMMCD <charlesmichaelmcdonald@gmail.com>
2015-12-27 13:37:21 -0500

    Updated javadocs.

--- SnorlaxJava/snorlax.java
+++ SnorlaxJava/snorlax.java
@@ -1,8 +1,8 @@
-/* Snorlax is an advanced sorting algorithm with extremely high efficiency
- * Note that this high efficiency comes with certain accuracy drawbacks outlined in the readme
+/** Snorlax is an advanced sorting algorithm with extremely high efficiency
+ *  Note that this high efficiency comes with certain accuracy drawbacks outlined in the readme
  * 
- * @author github.com/CMMCD/ 
- * @version 1.3
+ *  @author github.com/CMMCD/ 
+ *  @version 1.3
  */
 
 public class snorlax {
@@ -10,27 +10,27 @@ public class snorlax {
   // Your Snorlax's name.
   private String name;
   
-  /* Class constructor.
+  /** Class constructor.
    * 
-   * @param name What you choose to name your Snorlax.
+   *  @param name What you choose to name your Snorlax.
    */
   public snorlax(String name){
     this.name = name;
   }
   
-  /* Snorlax's sorting algorithm.
+  /** Snorlax's sorting algorithm.
    * 
-   * @param snorlax Integer Array passed to Snorlax for sorting.
-   * @return The usualy* sorted input. *See readme for more information*
+   *  @param snorlax Integer Array passed to Snorlax for sorting.
+   *  @return The usualy* sorted input. *See readme for more information*
    */
   public int[] sort(int[] snorlax) {
     return snorlax;
   }
   
-  /* Snorlax's sorting algorithm, with output instead of return.
-   * Note that the output is usualy* the sorted input. *See readme for more information*
+  /** Snorlax's sorting algorithm, with output instead of return.
+   *  Note that the output is usualy* the sorted input. *See readme for more information*
    * 
-   * @param snorlax Integer Array passed to Snorlax for sorting.
+   *  @param snorlax Integer Array passed to Snorlax for sorting.
    */
   public void snorlaxSort(int[] snorlax) {
     int[] snorlaxSorted = sort(snorlax);
